766ffe3bcf64793349e8b4c3047bfd62634d1322
wanderson648 <testepedroca@gmail.com>
2023-10-10 00:40:50 -0300

    implementa controller

---
+++ src/main/java/br/com/petz/clientepet/application/api/ClienteController.java
@@ -0,0 +1,15 @@
+package br.com.petz.clientepet.application.api;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia] ClienteController - postCliente");
+        log.info("[finaliza] ClienteController - postCliente");
+        return null;
+    }
+}
