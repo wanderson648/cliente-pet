2bf94a316f26a979233b9eef202e271aea6ef021
wanderson648 <testepedroca@gmail.com>
2023-10-10 00:06:08 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -1,7 +1,6 @@
 package br.com.petz.clientepet.cliente.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,6 +18,8 @@ import java.util.UUID;
 @Entity
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
     private UUID idCliente;
     @NotBlank
     private String nomeCompleto;
@@ -40,7 +41,6 @@ public class Cliente {
 
     public Cliente(String nomeCompleto, String email, String celular, Sexo sexo,
                    LocalDate dataNascimento, String cpf, Boolean aceitaTermos) {
-        this.idCliente = UUID.randomUUID();
         this.nomeCompleto = nomeCompleto;
         this.email = email;
         this.celular = celular;
