91b60e9ff51a0a0326efb69af76201580efd7fcb
wanderson648 <testepedroca@gmail.com>
2023-10-13 17:37:36 -0300

    implementa conversao pet para petclienteresponse

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetApi.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetApi.java
@@ -18,4 +18,5 @@ public interface PetApi {
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     List<PetClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
+    
 }
--- src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteListResponse.java
@@ -1,11 +1,35 @@
 package br.com.petz.clientepet.pet.application.api;
 
 import br.com.petz.clientepet.pet.domain.Pet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import lombok.Value;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class PetClienteListResponse {
+    UUID idPet;
+    String nomePet;
+    TipoPet tipo;
+    String raca;
+    String pelagemCor;
+    LocalDate dataNascimento;
+
     public static List<PetClienteListResponse> converte(List<Pet> petsDoCliente) {
-        return null;
+        return petsDoCliente.stream()
+                .map(PetClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public PetClienteListResponse(Pet pet) {
+        this.idPet = pet.getIdPet();
+        this.nomePet = pet.getNomePet();
+        this.tipo = pet.getTipo();
+        this.raca = pet.getRaca();
+        this.pelagemCor = pet.getPelagemCor();
+        this.dataNascimento = pet.getDataNascimento();
     }
 }
