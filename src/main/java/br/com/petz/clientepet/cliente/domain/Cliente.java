ac5ea3d288fff21900300bab89595cfa02d4ffda
wanderson648 <testepedroca@gmail.com>
2023-10-13 12:19:13 -0300

    modifica cliente para nao permitir email e cpf duplicado

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -26,6 +26,7 @@ public class Cliente {
     private String nomeCompleto;
     @NotBlank
     @Email
+    @Column(unique = true)
     private String email;
     @NotBlank
     private String celular;
@@ -34,6 +35,7 @@ public class Cliente {
     @NotNull
     private LocalDate dataNascimento;
     @CPF
+    @Column(unique = true)
     private String cpf;
     @NotNull
     private Boolean aceitaTermos;
